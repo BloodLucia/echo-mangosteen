5e38bc73a20e0dc6ee2fbf96c3d80e8bc0660abe
迷惘 <thecvcoder@foxmail.com>
2023-10-08 18:31:49 +0000

    创建UserController，添加Login方法

---
+++ internal/controller/user_controller.go
@@ -0,0 +1,19 @@
+package controller
+
+import "github.com/labstack/echo/v4"
+
+type userController struct {
+}
+
+// Login implements UserController.
+func (*userController) Login(c echo.Context) error {
+	return c.JSON(200, "login!")
+}
+
+type UserController interface {
+	Login(c echo.Context) error
+}
+
+func NewUserController() UserController {
+	return &userController{}
+}
