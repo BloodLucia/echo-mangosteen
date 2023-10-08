7c4aa9f7c2b429f9a666d1cfe72cb9d4cca191d2
迷惘 <thecvcoder@foxmail.com>
2023-10-08 19:00:04 +0000

    创建User模型

---
+++ internal/model/user.go
@@ -0,0 +1,18 @@
+package model
+
+import (
+	"database/sql"
+	"time"
+)
+
+type User struct {
+	ID        uint64       `xorm:"not null pk autoincr BIGINT(20) id"`
+	CreatedAt time.Time    `xorm:"created TIMESTAMP created_at"`
+	UpdatedAt time.Time    `xorm:"updated TIMESTAMP updated_at"`
+	DeletedAt sql.NullTime `xorm:"TIMESTAMP deleted_at"`
+	Email     string       `xorm:"not null VARCHAR(100) UNIQUE email"`
+}
+
+func (User) TableName() string {
+	return "users"
+}
