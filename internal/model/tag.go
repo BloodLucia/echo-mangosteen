20bd7e68ff5ea0164017f69ec1610e383ecf9acb
3lur <111719756+3lur@users.noreply.github.com>
2023-10-11 01:24:18 +0000

    创建TagModel

---
+++ internal/model/tag.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/spf13/cast"
+)
+
+type Tag struct {
+	ID        uint64       `xorm:"not null pk autoincr BIGINT(20) id"`
+	CreatedAt time.Time    `xorm:"created TIMESTAMP created_at"`
+	UpdatedAt time.Time    `xorm:"updated TIMESTAMP updated_at"`
+	DeletedAt sql.NullTime `xorm:"TIMESTAMP deleted_at"`
+	UserId    uint64       `xorm:"not null BIGINT(20) user_id"`
+	Name      string       `xorm:"not null varchar(50) name"`
+	Type      string       `xorm:"not null VARCHAR(10) type"`
+	Sign      []byte       `xorm:"not null CHAR(1) sign"`
+}
+
+func (t *Tag) TableName() string {
+	return "tags"
+}
+
+func (t *Tag) GetStringID() string {
+	return cast.ToString(t.ID)
+}
