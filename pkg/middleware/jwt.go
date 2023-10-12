15035e5df51b9fb6d6f811943abb8acec863075d
3lur <111719756+3lur@users.noreply.github.com>
2023-10-12 05:23:14 +0000

    修改JWT中间件, 采用路径白名单的方式跳过鉴权

--- pkg/middleware/jwt.go
+++ pkg/middleware/jwt.go
@@ -4,6 +4,7 @@ import (
 	"echo-mangosteen/pkg/errors"
 	"echo-mangosteen/pkg/jwt"
 	"echo-mangosteen/pkg/response"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,7 +12,9 @@ import (
 func JWTMiddleware(j *jwt.JWT) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			if c.Request().URL.Path == "/api/v1/login" {
+			whiteList := []string{"/login", "/ping"}
+			requestPATH := c.Request().URL.Path
+			if contains(whiteList, strings.TrimPrefix(requestPATH, "/api/v1")) {
 				return next(c)
 			}
 			tokenString := c.Request().Header.Get("Authorization")
@@ -27,4 +30,13 @@ func JWTMiddleware(j *jwt.JWT) echo.MiddlewareFunc {
 			return next(c)
 		}
 	}
+}
+
+func contains(slice []string, element interface{}) bool {
+	for _, v := range slice {
+		if v == element {
+			return true
+		}
+	}
+	return false
 }
