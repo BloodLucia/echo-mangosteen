08e26dcfe13955a0797ee72234f3d48caf2573d3
3lur <111719756+3lur@users.noreply.github.com>
2023-10-12 08:39:29 +0000

    让所有实体都包含Model

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -1,21 +1,11 @@
 package model
 
-import (
-	"database/sql"
-	"time"
-
-	"github.com/spf13/cast"
-)
-
 type Tag struct {
-	ID        uint64       `xorm:"not null pk autoincr BIGINT(20) id"`
-	CreatedAt time.Time    `xorm:"created TIMESTAMP created_at"`
-	UpdatedAt time.Time    `xorm:"updated TIMESTAMP updated_at"`
-	DeletedAt sql.NullTime `xorm:"TIMESTAMP deleted_at"`
-	UserId    uint64       `xorm:"not null BIGINT(20) user_id"`
-	Name      string       `xorm:"not null varchar(50) name"`
-	Type      string       `xorm:"not null VARCHAR(10) type"`
-	Sign      string       `xorm:"not null CHAR(1) sign"`
+	Model
+	UserId uint64 `xorm:"not null BIGINT(20) user_id"`
+	Name   string `xorm:"not null varchar(50) name"`
+	Type   string `xorm:"not null VARCHAR(10) type"`
+	Sign   string `xorm:"not null CHAR(1) sign"`
 }
 
 type TagAddRequest struct {
@@ -26,8 +16,4 @@ type TagAddRequest struct {
 
 func (t *Tag) TableName() string {
 	return "tags"
-}
-
-func (t *Tag) GetStringID() string {
-	return cast.ToString(t.ID)
 }
--- internal/model/user.go
+++ internal/model/user.go
@@ -1,16 +1,8 @@
 package model
 
-import (
-	"time"
-
-	"github.com/spf13/cast"
-)
-
 type User struct {
-	ID        uint64    `xorm:"not null pk autoincr BIGINT(20) id"`
-	CreatedAt time.Time `xorm:"created TIMESTAMP created_at"`
-	UpdatedAt time.Time `xorm:"updated TIMESTAMP updated_at"`
-	Email     string    `xorm:"not null VARCHAR(100) UNIQUE email"`
+	Model
+	Email string `xorm:"not null VARCHAR(100) UNIQUE email"`
 }
 
 type UserLoginRequest struct {
@@ -19,7 +11,7 @@ type UserLoginRequest struct {
 }
 
 type UserLoginResponse struct {
-	UserId string  `json:"user_id"`
+	UserId string `json:"user_id"`
 	Token  string `json:"token"`
 }
 
@@ -29,8 +21,4 @@ type UserSendValidationCodeRequest struct {
 
 func (u *User) TableName() string {
 	return "users"
-}
-
-func (u *User) GetStringID() string {
-	return cast.ToString(u.ID)
 }
