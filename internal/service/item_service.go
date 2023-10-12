dd33e59cdbadfe7adc80b2119815733d849953d3
3lur <111719756+3lur@users.noreply.github.com>
2023-10-12 11:37:43 +0000

    ItemService实现CreateItem方法

--- internal/service/item_service.go
+++ internal/service/item_service.go
@@ -1,12 +1,39 @@
 package service
 
-import "echo-mangosteen/internal/repo"
+import (
+	"context"
+	"echo-mangosteen/internal/model"
+	"echo-mangosteen/internal/repo"
+	"echo-mangosteen/pkg/errors"
+	"time"
+
+	"github.com/spf13/cast"
+)
 
 type itemService struct {
 	repo repo.ItemRepo
 }
 
+// CreateItem implements ItemService.
+func (srv *itemService) CreateItem(ctx context.Context, userId string, req *model.CreateItemRequest) error {
+	item := &model.Item{
+		UserId:     cast.ToUint64(userId),
+		TagId:      cast.ToUint64(req.TagId),
+		Kind:       model.Kind(req.Kind),
+		Amount:     req.Amount,
+		HappenedAt: time.Now(),
+	}
+
+	err := srv.repo.Create(ctx, item)
+	if err != nil {
+		return errors.InternalServer().WithMsg("创建失败, 请稍后再试~")
+	}
+
+	return nil
+}
+
 type ItemService interface {
+	CreateItem(ctx context.Context, userId string, req *model.CreateItemRequest) error
 }
 
 func NewItemService(repo repo.ItemRepo) ItemService {
