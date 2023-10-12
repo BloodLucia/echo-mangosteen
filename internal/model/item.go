9ed7175525029966e7dc0a0fa975504e9b854e51
3lur <111719756+3lur@users.noreply.github.com>
2023-10-12 11:19:49 +0000

    完善Item实体

--- internal/model/item.go
+++ internal/model/item.go
@@ -7,14 +7,18 @@ import (
 	"github.com/spf13/cast"
 )
 
+type Kind string
+
 type Item struct {
-	ID        uint64       `xorm:"not null pk autoincr BIGINT(20) id"`
-	CreatedAt time.Time    `xorm:"created TIMESTAMP created_at"`
-	UpdatedAt time.Time    `xorm:"updated TIMESTAMP updated_at"`
-	DeletedAt sql.NullTime `xorm:"TIMESTAMP deleted_at"`
-	TagId     uint64
-	UserId    uint64
-	// TODO
+	ID         uint64       `xorm:"not null pk autoincr BIGINT(20) id"`
+	CreatedAt  time.Time    `xorm:"created TIMESTAMP created_at"`
+	UpdatedAt  time.Time    `xorm:"updated TIMESTAMP updated_at"`
+	DeletedAt  sql.NullTime `xorm:"TIMESTAMP deleted_at"`
+	UserId     uint64       `xorm:"not null BIGINT(20) user_id"`
+	TagId      uint64       `xorm:"not null BIGINT(20) tag_id"`
+	Kind       Kind         `xorm:"not null VARCHAR(10) kind"`
+	Amount     int          `xorm:"not null DECIMAL(2) amount"`
+	HappenedAt time.Time    `xorm:"not null DATETIME happened_at"`
 }
 
 func (i *Item) TableName() string {
