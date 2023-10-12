bb3da0313483cde4dd5f45e8ce007cd1b9cdc798
3lur <111719756+3lur@users.noreply.github.com>
2023-10-12 11:33:07 +0000

    ItemRepo实现Create方法

--- internal/repo/item_repo.go
+++ internal/repo/item_repo.go
@@ -1,12 +1,27 @@
 package repo
 
-import "echo-mangosteen/internal/data"
+import (
+	"context"
+	"echo-mangosteen/internal/data"
+	"echo-mangosteen/internal/model"
+	"echo-mangosteen/pkg/errors"
+)
 
 type itemRepo struct {
 	*data.Data
 }
 
+// Create implements ItemRepo.
+func (repo *itemRepo) Create(ctx context.Context, item *model.Item) error {
+	if _, err := repo.Data.DB.Context(ctx).Insert(item); err != nil {
+		return errors.InternalServer()
+	}
+
+	return nil
+}
+
 type ItemRepo interface {
+	Create(ctx context.Context, item *model.Item) error
 }
 
 func NewItemRepo(data *data.Data) ItemRepo {
